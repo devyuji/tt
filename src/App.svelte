cf058a671d00a6d601b545298901ee80c2efb9c7
Raghavan <36996970+devyuji@users.noreply.github.com>
2024-09-28 10:40:12 +0530

    list completed

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import torrentList from "./lib/torrentlist";
+</script>
+
+<main>
+  <div class="container">
+    <nav class="navbar">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/devyuji/tt"
+            target="_blank"
+            rel="noopener noreferrer">Add / Edit</a
+          >
+        </li>
+      </ul>
+    </nav>
+    <div class="info--container">
+      <h1>List of torrent websites that work.</h1>
+      <h2>** Make sure to use VPN when using torrent websites **</h2>
+    </div>
+
+    <div class="link--container">
+      <ol>
+        {#each torrentList as list (list.id)}
+          <li>
+            <a href={list.url} target="_blank" rel="noopener noreferrer"
+              >{list.name}</a
+            >
+          </li>
+        {/each}
+      </ol>
+    </div>
+
+    <div class="information--container">
+      <p>
+        List will be updated regulary so that you get the right torrent website.
+      </p>
+    </div>
+  </div>
+</main>
---
+++ src/lib/torrentlist.ts
@@ -0,0 +1,50 @@
+interface listType {
+  id: number;
+  name: string;
+  url: string;
+}
+
+const list: listType[] = [
+  {
+    id: 1,
+    name: "ThePirateBay",
+    url: "https://thepiratebay.org",
+  },
+  {
+    id: 2,
+    name: "1337x",
+    url: "https://1337x.to",
+  },
+  {
+    id: 3,
+    name: "TorrentGalaxy",
+    url: "https://torrentgalaxy.one",
+  },
+  {
+    id: 4,
+    name: "YTS",
+    url: "https://yts.mx",
+  },
+  {
+    id: 5,
+    name: "MagnetDL",
+    url: "https://magnetdl.hair/",
+  },
+  {
+    id: 6,
+    name: "EZTV",
+    url: "https://eztvx.to/",
+  },
+  {
+    id: 7,
+    name: "NyaaSi",
+    url: "https://nyaa.si",
+  },
+  {
+    id: 8,
+    name: "XXXClub",
+    url: "https://xxxclub.to/",
+  },
+];
+
+export default list;
